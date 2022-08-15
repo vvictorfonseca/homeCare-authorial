ad0ee996d544354b593e03952836d2b41279e4fd
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-08-15 17:24:17 -0300

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app.js"
 
 dotenv.config({ path: ".env "});
 
-const PORT:Number = +process.env.PORT || 4000;
+const PORT:Number = +process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}.`);
