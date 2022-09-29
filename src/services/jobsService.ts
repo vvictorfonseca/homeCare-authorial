8935d73ea7070e7ff3254f80b9e705b8fd8c40c5
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-09-29 18:32:09 -0300

    fix: get jobs to evaluate

--- src/services/jobsService.ts
+++ src/services/jobsService.ts
@@ -1,66 +1,77 @@
 import dayjs from "dayjs";
+import moment from 'moment';
 
 import { jobs } from "@prisma/client";
 import clientReposiotory from "../repositories/clientsRepository.js";
 import jobsRepository from "../repositories/jobsRepository.js";
 import professionalRepository from "../repositories/professionalRepository.js";
+import { date } from "joi";
 
 export type CreateJobData = Omit<jobs, "id" | "isConfirmed">
 
 async function requestNewJob(newJob: CreateJobData) {
 
-    const professional = await professionalRepository.getProfessionalById(newJob.professionalId)
+  const professional = await professionalRepository.getProfessionalById(newJob.professionalId)
 
-    if(!professional) {
-        throw { type: "not_found", message: "this professional is not registered" }
-    }
+  if (!professional) {
+    throw { type: "not_found", message: "this professional is not registered" }
+  }
 
-    await jobsRepository.requestNewJob(newJob)
+  await jobsRepository.requestNewJob(newJob)
 }
 
 async function updateRequestToTrue(newJob: CreateJobData) {
 
-    const client = clientReposiotory.getClientById(newJob.clientId)
-    const professional = professionalRepository.getProfessionalById(newJob.professionalId)
+  const client = clientReposiotory.getClientById(newJob.clientId)
+  const professional = professionalRepository.getProfessionalById(newJob.professionalId)
 
-    if(!client || !professional) {
-        throw { type: "bad_request", message: "professional or client nonexist" }
-    }
+  if (!client || !professional) {
+    throw { type: "bad_request", message: "professional or client nonexist" }
+  }
 
-    await jobsRepository.updateRequestToTrue(newJob)
+  await jobsRepository.updateRequestToTrue(newJob)
 }
 
 async function getjobsByProfessionalId(professionalId: number) {
-    const jobs = await jobsRepository.getjobsByProfessionalId(professionalId)
+  const jobs = await jobsRepository.getjobsByProfessionalId(professionalId)
 
-    return jobs
+  return jobs
 }
 
 async function getJobsToEvaluateByClientId(clientId: number) {
-  console.log("entrooou")
-  const jobs = await  jobsRepository.getJobsByClientId(clientId)
+  const jobs = await jobsRepository.getJobsByClientId(clientId)
 
-  const jobsFiltered = jobs.filter(element => element.date < dayjs().format('DD/MM/YYYY'));
+  function getJobs(value) {
+    let date = value.date
+    let dateParts = date.split("/")
+    var dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
+
+    if (dayjs().isAfter(dateObject)) {
+      return value
+    }
+  }
+
+  const jobsFiltered = jobs.filter(getJobs)
 
   return jobsFiltered
 }
 
 async function getJobsByClientId(clientId: number) {
-    const jobs = await  jobsRepository.getJobsByClientId(clientId)
-    return jobs
+  const jobs = await jobsRepository.getJobsByClientId(clientId)
+  return jobs
 }
 
 async function deleteJobById(jobId: number) {
-    await jobsRepository.deleteJobById(jobId)
+  await jobsRepository.deleteJobById(jobId)
 }
 
 const jobsService = {
-    requestNewJob,
-    updateRequestToTrue,
-    getjobsByProfessionalId,
-    getJobsByClientId,
-    deleteJobById,
-    getJobsToEvaluateByClientId
+  requestNewJob,
+  updateRequestToTrue,
+  getjobsByProfessionalId,
+  getJobsByClientId,
+  deleteJobById,
+  getJobsToEvaluateByClientId
 }
 
 export default jobsService
