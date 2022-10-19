2add57566ed015c711b4fe0a3bb9c0490ef47627
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-10-19 18:37:23 -0300

    fix: get professional by id

--- src/routers/professionalRouter.ts
+++ src/routers/professionalRouter.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import validateToken from "../middlewares/validateToken.js";
 import {professionalSchema, loginProfessionalSchema, updateDescriptionSchema} from "../schemas/professioanlService.js";
-import { createProfessional, loginProfessional, getProfessionalsByType, updateProfessionalDescription } from "../controllers/professionalController.js";
+import { createProfessional, loginProfessional, getProfessionalsByType, updateProfessionalDescription, getProfessionalById } from "../controllers/professionalController.js";
 
 const professionalRouter = Router()
 
@@ -11,6 +11,6 @@ professionalRouter.post("/sign-up/professional", validateSchema(professionalSche
 professionalRouter.post("/sign-in/professional", validateSchema(loginProfessionalSchema), loginProfessional)
 professionalRouter.get("/professionals/:type",  validateToken, getProfessionalsByType)
 professionalRouter.put("/update/description", validateToken, validateSchema(updateDescriptionSchema), updateProfessionalDescription)
-professionalRouter.get("/professional/:professionalId")
+professionalRouter.get("/professional/:professionalId", validateSchema, getProfessionalById)
 
 export default professionalRouter
