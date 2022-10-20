08afcd9b4df1085b73b8417fd341ed8a57d7412f
vvictorfonseca <vvictor.h.fonseca@gmail.com>
2022-10-20 16:23:31 -0300

    update getJobsByClientId to order by id

--- src/repositories/jobsRepository.ts
+++ src/repositories/jobsRepository.ts
@@ -2,182 +2,187 @@ import prisma from "../config/database.js";
 import { CreateJobData, CreateEvaluateData } from "../services/jobsService.js";
 
 async function requestNewJob(newJob: CreateJobData) {
-    await prisma.jobs.create({data: newJob})
+  await prisma.jobs.create({ data: newJob })
 }
 
 async function updateRequestToTrue(newJob: CreateJobData) {
-    const updateJob = await prisma.jobs.updateMany({
-        where: {
-            clientId: newJob.clientId,
-            professionalId: newJob.professionalId,
-            date: newJob.date
-        },
-        data: {
-            isConfirmed: "Confirmed"
-        }
-    })
+  const updateJob = await prisma.jobs.updateMany({
+    where: {
+      clientId: newJob.clientId,
+      professionalId: newJob.professionalId,
+      date: newJob.date
+    },
+    data: {
+      isConfirmed: "Confirmed"
+    }
+  })
 
-    return updateJob
+  return updateJob
 }
 
 async function updateRequestToDone(newJob: CreateJobData) {
   const updateJob = await prisma.jobs.updateMany({
-      where: {
-          clientId: newJob.clientId,
-          professionalId: newJob.professionalId,
-          date: newJob.date
-      },
-      data: {
-          isConfirmed: "Done"
-      }
+    where: {
+      clientId: newJob.clientId,
+      professionalId: newJob.professionalId,
+      date: newJob.date
+    },
+    data: {
+      isConfirmed: "Done"
+    }
   })
 
   return updateJob
 }
 
 async function getjobsByProfessionalId(professionalId: number) {
-    const jobs = await prisma.jobs.findMany({
-        where: {
-            professionalId
-        },
-        
-        select: {
-            id: true,
-            date: true,
-            isConfirmed: true,
-            
-            clients: {
-                select: {
-                    id: true,
-                    fullName: true,
-                    phoneNumber: true,
-                    profilePhoto: true,
-                    
-                    address: {
-                        select: {
-                            clientId: true,
-                            city: true,
-                            district: true,
-                            street: true,
-                            number: true,
-                            complement: true,
-                            zipCode: true
-                        }
-                    }
-                }
-            },
-            
-            professionals: {
-                select: {
-                    id: true,
-                    fullName: true,
-                    city: true,
-                    type: true
-                }
-            }
-        }
-    });
+  const jobs = await prisma.jobs.findMany({
+    where: {
+      professionalId
+    },
 
-    return jobs
+    select: {
+      id: true,
+      date: true,
+      isConfirmed: true,
+
+      clients: {
+        select: {
+          id: true,
+          fullName: true,
+          phoneNumber: true,
+          profilePhoto: true,
+
+          address: {
+            select: {
+              clientId: true,
+              city: true,
+              district: true,
+              street: true,
+              number: true,
+              complement: true,
+              zipCode: true
+            }
+          }
+        }
+      },
+
+      professionals: {
+        select: {
+          id: true,
+          fullName: true,
+          city: true,
+          type: true
+        }
+      }
+    }
+  });
+
+  return jobs
 }
 
 async function getJobsByClientId(clientId: number) {
-    const jobs = await prisma.jobs.findMany({
-        where: {
-            clientId,
-        },
+  const jobs = await prisma.jobs.findMany({
+    orderBy: {
+      id: 'desc'
+    },
+    where: {
+      clientId,
+    },
 
+    select: {
+      id: true,
+      date: true,
+      isConfirmed: true,
+
+      clients: {
         select: {
-            id: true,
-            date: true,
-            isConfirmed: true,
-            
-            clients: {
-                select: {
-                    id: true,
-                    fullName: true,
-                    phoneNumber: true,
-                    
-                    address: {
-                        select: {
-                            clientId: true,
-                            city: true,
-                        }
-                    }
-                }
-            },
-            
-            professionals: {
-                select: {
-                    id: true,
-                    fullName: true,
-                    city: true,
-                    type: true,
-                    phoneNumber: true,
-                    profilePhoto: true,
-                    description: true
-                }
-            }
-        }
-    });
+          id: true,
+          fullName: true,
+          phoneNumber: true,
 
-    return jobs
+          address: {
+            select: {
+              clientId: true,
+              city: true,
+            }
+          }
+        }
+      },
+
+      professionals: {
+        select: {
+          id: true,
+          fullName: true,
+          city: true,
+          type: true,
+          phoneNumber: true,
+          profilePhoto: true,
+          description: true
+        }
+      }
+    }
+  });
+
+  return jobs
 }
 
 async function getJobsByClientIdToEvaluate(clientId: number) {
   const jobs = await prisma.jobs.findMany({
-      where: {
-          clientId,
-          isConfirmed: "Confirmed"
+    where: {
+      clientId,
+      isConfirmed: "Confirmed"
+    },
+
+    select: {
+      id: true,
+      date: true,
+      isConfirmed: true,
+
+      clients: {
+        select: {
+          id: true,
+          fullName: true,
+          phoneNumber: true,
+
+          address: {
+            select: {
+              clientId: true,
+              city: true,
+            }
+          }
+        }
       },
 
-      select: {
+      professionals: {
+        select: {
           id: true,
-          date: true,
-          isConfirmed: true,
-          
-          clients: {
-              select: {
-                  id: true,
-                  fullName: true,
-                  phoneNumber: true,
-                  
-                  address: {
-                      select: {
-                          clientId: true,
-                          city: true,
-                      }
-                  }
-              }
-          },
-          
-          professionals: {
-              select: {
-                  id: true,
-                  fullName: true,
-                  city: true,
-                  type: true,
-                  phoneNumber: true,
-                  profilePhoto: true,
-                  description: true
-              }
-          }
+          fullName: true,
+          city: true,
+          type: true,
+          phoneNumber: true,
+          profilePhoto: true,
+          description: true
+        }
       }
+    }
   });
 
   return jobs
 }
 
 async function deleteJobById(jobId: number) {
-    const job = await prisma.jobs.delete({
-        where: {id: jobId}
-    })
+  const job = await prisma.jobs.delete({
+    where: { id: jobId }
+  })
 
-    return job
+  return job
 }
 
 async function evaluateJob(evaluateJob: CreateEvaluateData) {
-  await prisma.evaluations.create({data: evaluateJob})
+  await prisma.evaluations.create({
+    data: evaluateJob
+  })
 }
 
 async function getProfessionalEvaluations(professionalId: number) {
@@ -187,7 +192,7 @@ async function getProfessionalEvaluations(professionalId: number) {
         professionalId: professionalId
       }
     },
-    
+
     select: {
       content: true,
 
@@ -219,15 +224,15 @@ async function getProfessionalEvaluations(professionalId: number) {
 }
 
 const jobsRepository = {
-    requestNewJob,
-    updateRequestToTrue,
-    updateRequestToDone,
-    getjobsByProfessionalId,
-    getJobsByClientId,
-    getJobsByClientIdToEvaluate,
-    deleteJobById,
-    evaluateJob,
-    getProfessionalEvaluations
+  requestNewJob,
+  updateRequestToTrue,
+  updateRequestToDone,
+  getjobsByProfessionalId,
+  getJobsByClientId,
+  getJobsByClientIdToEvaluate,
+  deleteJobById,
+  evaluateJob,
+  getProfessionalEvaluations
 }
 
 export default jobsRepository
